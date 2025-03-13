66b4ac42a9550afa154e09a3c65332ec37d5f001
kd <erm.melnik@gmail.com>
2025-03-13 10:34:45 +0100

    reorganizing folders and files structure

--- src/script_add_lessons_list.js
+++ src/script_add_lessons_list.js
@@ -1,8 +1,15 @@
-fetch("src/lessonsList.html")
-  .then((response) => response.text())
-  .then((data) => {
-    document.getElementById("lessons-list-container").innerHTML = data;
-  })
-  .catch((error) => {
-    console.error("Ошибка при загрузке файла:", error);
-  });
+function loadFile(url, containerId) {
+  fetch(url)
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById(containerId).innerHTML = data;
+    })
+    .catch((error) => {
+      console.error("Ошибка при загрузке файла:", url, error);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadFile("src/lessonsList.html", "lessons-list-container");
+  loadFile("src/homeworkList.html", "homework-list-container");
+});
