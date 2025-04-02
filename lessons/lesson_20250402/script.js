0da6166a53f1403303e2006e5fb504948a9c8ed0
kd <erm.melnik@gmail.com>
2025-04-02 12:30:24 +0200

    lesson 02.04.2025

--- lessons/lesson_20250402/script.js
+++ lessons/lesson_20250402/script.js
@@ -1,73 +1,39 @@
-// ! асинхронность в JS
+const gridContainer = document.querySelector(".grid-container");
 
-// setTimeout(() => {
-//   console.log("Прошло 9 секунд");
-// }, 9000);
+// ! Реализация на синтаксисе .then()
 
-const interval = setInterval(() => {
-  // console.log("Ping!");
-
-  setTimeout(() => {
-    // console.log("Pong!");
-  }, 2000);
-}, 3000);
-
-setTimeout(() => {
-  clearInterval(interval);
-}, 9000);
-
-// ! async requests & promises
-
-// API - Application Programming Interface
-// набор правил позволяющий одним программам понимать другие программы, общаться друг с другом и обмениваться данными
-
-// часто когда мы работаем с API мы хотим сделать запрос за данными чтобы обработать их в своем коде и отобразить на странице
-
-// * если я хочу получить удаленные данные по запросу - то используем ключевое слово fetch() (в переводе 'принеси')
-
-// в ответ на запрос мне приходят данные в виде объекта Promise
-
-// Promise - это особый вид данных в JS который нуждается в асинхронной обработке. У него есть три состояния:
-
-// 1. <Pending> - ожидание данных
-// 2. <Fulfilled> - данные успешно пришли
-// 3. <Rejected> - данные пришли с ошибкой
-
-// ? запрос не получится обработать синхронно
-
-// const data = fetch('https://dog.ceo/api/breeds/image/random')
-// console.log(data)
-
-// ? чтобы обработать асинхронный запрос возвращающий объект Promise мы используем метод then()
-
-// ! короткий fetch запрос только про успех
-
-// fetch("https://dog.ceo/api/breed")
+// fetch("https://rickandmortyapi.com/api/character?page=1")
 //   .then((res) => res.json())
 //   .then((data) => {
-//     const img = document.createElement("img");
-//     img.src = data.message;
-//     document.body.append(img);
-//   })
+//     data.results.map((character) => {
+//       console.log(character);
+//       const section = document.createElement("section");
+//       section.textContent = `${character.name} \n from (${character.location.name})`;
+//       document.body.append(section);
+//     });
+//   });
 
-// ! длинный fetch запрос обрабатывающий ошибку
+// ! Реализация на синтаксисе async / await (асинхронные функции)
 
-// запрос за данными возвращающий promise который мы обрабатываем через then()
-fetch("https://dog.ceo/api/breeds/image/random")
-  .then((res) => {
-    // если поле ok - в положении false мы переходим к блоку catch отрабатывающего ошибку
-    if (!res.ok) {
-      throw new Error(`Ошибка: ${res.status}`);
-    }
-    return res.json();
-  })
-  // если все хорошо обрабатываем данные
-  .then((data) => {
+async function getCharacters() {
+  const res = await fetch("https://rickandmortyapi.com/api/character?page=1");
+  const data = await res.json();
+
+  data.results.map((character) => {
+    const section = document.createElement("section");
+    const h2 = document.createElement("h2");
+    h2.textContent = `${character.name} \n from (${character.location.name})`;
+    section.append(h2);
+
+    const p = document.createElement("p");
+    p.textContent = `Status: ${character.species}, status: ${character.status}`;
+    section.append(p);
+
     const img = document.createElement("img");
-    img.src = data.message;
-    document.body.append(img);
-  })
-  // если ошибка показываем ее в консоли
-  .catch((error) => {
-    console.log(error);
+    img.src = character.image;
+    section.append(img);
+
+    gridContainer.append(section);
   });
+}
+getCharacters();
