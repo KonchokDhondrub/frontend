1241fb70c832e82e6e3f8960391234af493212c6
kd <erm.melnik@gmail.com>
2025-04-07 17:03:18 +0200

    fixing

--- lessons/lesson_20250407/js/news.js
+++ lessons/lesson_20250407/js/news.js
@@ -1,19 +1,21 @@
-const gridContainer = document.querySelector('.grid-container')
+const gridContainer = document.querySelector(".grid-container");
 
 const getNews = async () => {
   const res = await fetch("https://newsapi.org/v2/everything?q=zoo+otter&from=2025-03-07&sortBy=publishedAt&apiKey=b99bfac3612f40f6bb28dfab990cde8d");
   const data = await res.json();
-  data.articles.slice(1,12).map((el) => {
-    const card = document.createElement('section')
+  data.articles.slice(1, 12).map((el) => {
+    const card = document.createElement("section");
     const p = document.createElement("p");
     p.textContent = el.title;
-    const img = document.createElement('img')
-    img.src = el.urlToImage
-    const a = document.createElement('a')
-    a.href = el.url
-    a.target = '_blank'
-    a.textContent = 'link to source'
-    card.append(p, img, a)
-    gridContainer.append(card)
+    const img = document.createElement("img");
+    img.src = el.urlToImage || "fallback.png";
+    const a = document.createElement("a");
+    a.href = el.url;
+    a.target = "_blank";
+    a.textContent = "link to source";
+    card.append(p, img, a);
+    gridContainer.append(card);
   });
 };
+
+getNews();
